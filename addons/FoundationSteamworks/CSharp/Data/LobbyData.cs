f64c872570757b8a4a9d6d5b46c3e2c5e5aeb7be
James McGhee <jamesmcghee@heathen.group>
2026-05-09 16:56:57 +0100

    Cleaned up over work on Lobby structures

--- addons/FoundationSteamworks/CSharp/Data/LobbyData.cs
+++ addons/FoundationSteamworks/CSharp/Data/LobbyData.cs
@@ -1,6 +1,4 @@
 using Godot;
-using System;
-using System.Collections.Generic;
 
 namespace Heathen.SteamworksIntegration
 {
@@ -16,106 +14,10 @@ namespace Heathen.SteamworksIntegration
 
         public string HexId => (string)_instance.Get("HexId");
 
-        public string LobbyName
-        {
-            get => (string)_instance.Call("GetLobbyName");
-            set => _instance.Call("SetLobbyName", value);
-        }
-
-        public bool IsFull => (bool)_instance.Get("Full");
-
-        public bool IsOwner => (bool)_instance.Call("IsOwner");
-
-        public bool IsMember => (bool)_instance.Call("IsMember");
-
-        public int MemberCount => (int)_instance.Get("MemberCount");
-
-        public int MaxMembers
-        {
-            get => (int)_instance.Call("GetMaxMembers");
-            set => _instance.Call("SetMaxMembers", value);
-        }
-
-        public LobbyUseHint UseHint
-        {
-            get => (LobbyUseHint)(int)_instance.Call("GetUseHint");
-            set => _instance.Call("SetUseHint", (int)value);
-        }
-
-        public LobbyType Type
-        {
-            get => (LobbyType)(int)_instance.Call("GetType");
-            set => _instance.Call("SetType", (int)value);
-        }
-
-        public string this[string key]
-        {
-            get => (string)_instance.Call("GetLobbyData", key);
-            set => _instance.Call("SetLobbyData", key, value);
-        }
-
         public static LobbyData FromHex(string hexId)
         {
             Variant result = ClassDB.ClassCallStatic("LobbyData", "FromHex", hexId);
             return result.Obj is GodotObject obj ? new LobbyData(obj) : null;
         }
-
-        public static void CreateLobby(LobbyType type, LobbyUseHint hint, int maxMembers, Action<LobbyData, SteamResult, bool> callback) =>
-            SteamTools.CreateLobby(type, hint, maxMembers, callback);
-
-        public void JoinLobby(Action<LobbyData, LobbyEnterResponse, bool> callback) =>
-            SteamTools.JoinLobby(this, callback);
-
-        public static void JoinLobbyById(ulong id, Action<LobbyData, LobbyEnterResponse, bool> callback) =>
-            SteamTools.JoinLobbyById(id, callback);
-
-        public static void JoinLobbyByHex(string hexId, Action<LobbyData, LobbyEnterResponse, bool> callback) =>
-            SteamTools.JoinLobbyByHex(hexId, callback);
-
-        public void LeaveLobby() => SteamTools.LeaveLobby(this);
-
-        public void SetMemberData(string key, string value) => _instance.Call("SetMemberData", key, value);
-
-        public string GetMemberData(string key)
-        {
-            var nativeUser = UserData.Me.ToGDNative();
-            return (string)_instance.Call("GetMemberData", nativeUser, key);
-        }
-
-        public string GetMemberData(UserData user, string key) =>
-            (string)_instance.Call("GetMemberData", user.ToGDNative(), key);
-
-        public void SetOwner(UserData user) => _instance.Call("SetOwner", user.ToGDNative());
-
-        public List<UserData> GetMemberList()
-        {
-            Variant result = _instance.Call("GetMemberList");
-            var list = new List<UserData>();
-            if (result.VariantType != Variant.Type.Array)
-                return list;
-            foreach (Variant item in (Godot.Collections.Array)result)
-                if (item.Obj is GodotObject obj)
-                    list.Add(new UserData(obj));
-            return list;
-        }
-
-        public void ActivateInviteDialog() => _instance.Call("ActivateInviteDialog");
-
-        public void ActivateRemotePlayTogetherInviteDialog() => _instance.Call("ActivateRemotePlayTogetherInviteDialog");
-
-        public void SetJoinable(bool joinable) => _instance.Call("SetJoinable", joinable);
-
-        public void SetListenServer() => _instance.Call("SetListenServer");
-
-        public void SetDedicatedServer(ulong serverId, string ip, ushort port) =>
-            _instance.Call("SetDedicatedServer", serverId, ip, port);
-
-        public bool HasGameServer() => (bool)_instance.Call("HasGameServer");
-
-        public ulong GetServerId() => (ulong)_instance.Call("GetServerId");
-
-        public string GetServerIp() => (string)_instance.Call("GetServerIp");
-
-        public ushort GetServerPort() => (ushort)_instance.Call("GetServerPort");
     }
 }
--- addons/FoundationSteamworks/src/public/LobbyData.h
+++ addons/FoundationSteamworks/src/public/LobbyData.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "SteamApi.h"
-#include "LobbyType.h"
 
 class LobbyData : public RefCounted
 {
@@ -16,47 +15,12 @@ public:
     LobbyData(CSteamID steam_id) : id(steam_id) {}
     LobbyData(uint64 steam_id) : id(steam_id) {}
 
-    void InviteToLobby(const Ref<UserData> &user) const;
-    static void CreateLobby(LobbyType::Type type, LobbyUseHint::Hint hint, int maxMembers, const Callable &callback);
-    void JoinLobby(Callable callback);
-    static void JoinLobbyById(int64_t lobby_id, Callable callback);
-    static void JoinLobbyByHex(String lobby_id, Callable callback);
-    void LeaveLobby() const;
-    void SetLobbyData(const String &key, const String &value) const;
-    String GetLobbyData(const String &key) const;
-    void SetMemberData(const String &key, const String &value) const;
-    String GetMemberData(const Ref<UserData> &user, const String &key) const;
-    String GetLobbyName() const;
-    void SetLobbyName(const String &name) const;
-    int GetMemberCount() const;
-    int GetMaxMembers() const;
-    void SetMaxMembers(int max_members) const;
-    LobbyUseHint::Hint GetUseHint() const;
-    void SetUseHint(LobbyUseHint::Hint hint) const;
-    LobbyType::Type GetType() const;
-    void SetType(LobbyType::Type type) const;
-    bool IsFull() const;
-    bool IsOwner() const;
-    void SetOwner(Ref<UserData> user) const;
-    bool IsMember() const;
-    void SetJoinable(bool joinable) const;
-    void SetListenServer() const;
-    void SetDedicatedServer(uint64_t serverId, const String &ip, uint16_t port) const;
-    bool HasGameServer() const;
-    uint64_t GetServerId() const;
-    String GetServerIp() const;
-    uint16_t GetServerPort() const;
-    TypedArray<UserData> GetMemberList() const;
-    Ref<UserData> GetOwner() const;
-    bool SendChatMessage(const String &message) const;
     CSteamID ToCSteamID() const { return id; }
     uint64_t GetIntId() const;
     operator CSteamID() const;
     String ToHexId() const;
     static Ref<LobbyData> FromHex(String hexId);
     bool Equals(Ref<LobbyData> other) const;
-    void ActivateInviteDialog() const;
-    void ActivateRemotePlayTogetherInviteDialog() const;
 
     bool operator==(const LobbyData &other) const { return id == other.id; }
     bool operator!=(const LobbyData &other) const { return id != other.id; }
@@ -66,18 +30,4 @@ public:
     bool operator!=(uint64_t other) const { return GetIntId() != other; }
     bool operator<(const LobbyData &other) const { return GetIntId() < other.GetIntId(); }
     bool operator>(const LobbyData &other) const { return GetIntId() > other.GetIntId(); }
-
-    static constexpr auto DataName = "name";
-    static constexpr auto DataVersion = "z_heathenGameVersion";
-    static constexpr auto DataReady = "z_heathenReady";
-    static constexpr auto DataKick = "z_heathenKick";
-    static constexpr auto DataMode = "z_heathenMode";
-    static constexpr auto DataType = "z_heathenType";
-    static constexpr auto DataModeGeneral = "General";
-    static constexpr auto DataModeSession = "Session";
-    static constexpr auto DataModeParty = "Party";
-    static constexpr auto DataTypePrivate = "Private";
-    static constexpr auto DataTypeFriendOnly = "FriendOnly";
-    static constexpr auto DataTypePublic = "Public";
-    static constexpr auto DataTypeInvisible = "Invisible";
 };
